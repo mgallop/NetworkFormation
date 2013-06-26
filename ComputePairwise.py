9189df317ee027630e4f0b5d2559dfed0c0cc558
mgallop <max.gallop@gmail.com>
2013-06-26 15:02:15 -0400

    Update ComputePairwise.py
    Fixed memory issue in cutting links. Made utility function calculate more efficiently.

--- ComputePairwise.py
+++ ComputePairwise.py
@@ -5,12 +5,10 @@ import networkx as nx
 from itertools import *
 from math import log
 from math import hypot
-import matplotlib.pyplot as plt
+import time
 import csv
 import random
 import copy
-import numpy
-import matplotlib.cm as cm
 
 
 def frange(start, end=None, inc=None):
@@ -46,25 +44,41 @@ def graph_to_csv(filename, graph):
     for i in graph.nodes():
       my_writer.writerow({"name":i, "cap":graph.node[i]['cap'], "ideo": graph.node[i]['ideo']})
       
-
-      
 def powerset(iterable):
   s = list(iterable)
-  return list(chain.from_iterable(combinations(s, r) for r in range(len(s) + 1)))
+  return chain.from_iterable(combinations(s, r) for r in range(len(s) + 1))
 
+
+def powerset2(seq):
+  if len(seq) <= 1:
+    yield seq
+    yield []
+  else:
+    for item in powerset2(seq[1:]):
+      yield [seq(0)] + item
+      yield item
 
 ###CHANGE THIS FUNCTION IF YOU WANT TO CHANGE THE UTILITY FUNCTION  
 ## Anna Utility Function 3
-def utility(graph, delta = 1, c = 0):
+def utility2(graph,actor, delta = 1, c = 0):
   util = {}
   for i in graph.nodes():
     util[i] = 0
     for j in graph.nodes():
       if nx.has_path(graph, i, j):
-        util[i] += (graph.node[j]["cap"]*(1-abs(graph.node[i]["ideo"]-graph.node[j]["ideo"]))*delta**(nx.shortest_path_length(graph,i,j)))
+       util[i] += (graph.node[j]["cap"]*(1-abs(graph.node[i]["ideo"]-graph.node[j]["ideo"]))*delta**(nx.shortest_path_length(graph,i,j)))
     util[i] = (util[i])**.5 - (c*len(graph.neighbors(i)))
-  return util
+  return util[actor]
 
+def utility(graph, actor, delta, c):
+  Z = nx.shortest_path_length(graph, actor)
+  utilz = 0
+  for i in Z.keys():
+    utilz += (graph.node[i]["cap"]*(1-abs(graph.node[i]["ideo"]-graph.node[actor]["ideo"]))*delta**(Z[i]))
+  utilz **= .5
+  utilz -= c*nx.degree(graph, actor)
+  return utilz
+	
 
 def One_Add(graph, delta, c,  order = 3):
   if order == 3:
@@ -77,10 +91,12 @@ def One_Add(graph, delta, c,  order = 3):
       if j not in graph[i]:
         graphprime = copy.deepcopy(graph)
         graphprime.add_edge(i, j)
-        if utility(graphprime,delta, c)[i] > utility(graph, delta, c)[i] and utility(graphprime, delta, c)[j] > utility(graph, delta, c)[j]:
+        if utility(graphprime,i,delta, c) > utility(graph, i,delta, c) and utility(graphprime,j, delta, c) > utility(graph, j,delta, c):
           graph.add_edge(i,j)
             #print i, 'adds:      ', j
             
+
+			
 def One_Cut(graph, delta, c, order = 3): 
   if order == 3:
     h = graph.nodes()
@@ -91,7 +107,7 @@ def One_Cut(graph, delta, c, order = 3):
     for edges in graph.edges(i):
       x.append(edges[1])
     winner = copy.deepcopy(graph)
-    possibilities = list(powerset(x))#                  # all possible subsets of (i)'s neighbors
+    possibilities = powerset(x)#                  # all possible subsets of (i)'s neighbors
     graphprime = copy.deepcopy(winner)
     y = graph.edges(i)
     graphprime.remove_edges_from(y)
@@ -99,7 +115,7 @@ def One_Cut(graph, delta, c, order = 3):
     for j in possibilities:#                            # in each potential subgroup of i's neighbors
       for k in j:#                                      # for each member of that subgroup
         graphprime.add_edge(i, k)#                      # connect it to (i)
-      if utility(graphprime, delta, c)[i] > utility(winner, delta, c)[i]:
+      if utility(graphprime,i, delta, c) > utility(winner, i, delta, c):
         winner = copy.deepcopy(graphprime)
         #print i, 'winner' , j
       for k in j:
@@ -108,7 +124,7 @@ def One_Cut(graph, delta, c, order = 3):
     graph.add_edges_from(winner.edges())
     #print i, 'new neighbors:', winner[i]               
 
-	
+    
 
     
 def Find_Directed_Nash(graph): 
@@ -141,18 +157,29 @@ def Find_Directed_Nash(graph):
 
 
 def Find_Pairwise_Nash(graph, delta, c, orderb = 3):
+  path = []
   Done, Add, Cut = False, False, False
   while Done == False:
     if Add and Cut == True:
       Done = True
     while Add == False:
+      print "Adding:", time.clock()
       edges = graph.edges()
       One_Add(graph, delta, c, orderb)
       if graph.edges() == edges:
         Add = True
       else:
-        Cut = False
+        Cut = False ###Track outcomes, if improving cycle, break and retry with new order.
+        if nx.to_dict_of_lists(graph) in path:
+          Coin_Flip_Graph(graph, random.uniform(0,1))
+          print "Uh Oh, an improving cycle. Shuffling and trying again!"
+          Find_Pairwise_Nash(graph, delta, c)
+          Add, Cut = True, True
+          break
+        else:
+          path.append(nx.to_dict_of_lists(graph))
     while Cut == False:
+      print "Cutting:", time.clock()
       Add = False
       edges = graph.edges()
       One_Cut(graph, delta, c, orderb)
@@ -160,35 +187,43 @@ def Find_Pairwise_Nash(graph, delta, c, orderb = 3):
         Cut = True
       else:
         Add = False
+        if nx.to_dict_of_lists(graph) in path:
+          Coin_Flip_Graph(graph, random.uniform(0,1))
+          print "Uh Oh, an improving cycle. Shuffling and trying again!"
+          Find_Pairwise_Nash(graph, delta, c)
+          Add, Cut = True, True
+          break
+        else:
+          path.append(nx.to_dict_of_lists(graph))
+  return graph.copy()
         
 def Find_Many_Pairwise_Nashes(graph, delta, c, granularity):
   eqlist = []
+  figs = 0
   len = 1.0/granularity
   for l in frange(0,end = 1, inc = len):
     Coin_Flip_Graph(graph, l)
     Find_Pairwise_Nash(graph, delta, c)
     changedlist=[]
-    if eqlist == []:
+    if nx.to_dict_of_lists(graph) not in eqlist:
       eqlist.append(nx.to_dict_of_lists(graph))
-    else:
-      tru = 1
-      for i in eqlist:
-        if i == nx.to_dict_of_lists(graph):
-          tru *= 0
-      if tru == 1:
-        eqlist.append(nx.to_dict_of_lists(graph))
+      plt.clf()
+      nx.draw_circular(graph)
+      plt.draw()
+      plt.savefig("Eq" + str(figs)+".png")
+      figs += 1   
   return eqlist
     
-	
+    
 def Coin_Flip_Graph(graph, prob = .5):
   x = graph.nodes()
+  graph.remove_edges_from(graph.edges())
   for i in x:
     x = x[1:]
     for j in x:
       y = random.uniform(0,1)
       if y > prob:
         graph.add_edge(i,j)
-
 
 
 #T = grapher("Toy2.csv")
@@ -204,3 +239,89 @@ def Coin_Flip_Graph(graph, prob = .5):
 #c = 0
 #Find_Pairwise_Nash(X)
 #len(X.edges()) == len(T.edges())
+
+
+def One_Add2(graph, delta, c,  order = 3):
+  if order == 3:
+    h = graph.nodes()
+  else:
+    h = order
+  for i in h:
+    h = h[1:]
+    for j in h:
+      if j not in graph[i]:
+        graphprime = copy.deepcopy(graph)
+        graphprime.add_edge(i, j)
+        if utility2(graphprime,i,delta, c) > utility2(graph, i,delta, c) and utility2(graphprime,j, delta, c) > utility2(graph, j,delta, c):
+          graph.add_edge(i,j)
+            #print i, 'adds:      ', j
+            
+
+			
+def One_Cut2(graph, delta, c, order = 3): 
+  if order == 3:
+    h = graph.nodes()
+  else:
+    h = order
+  for i in h:
+    x = []
+    for edges in graph.edges(i):
+      x.append(edges[1])
+    winner = copy.deepcopy(graph)
+    possibilities = list(powerset(x))#                  # all possible subsets of (i)'s neighbors
+    graphprime = copy.deepcopy(winner)
+    y = graph.edges(i)
+    graphprime.remove_edges_from(y)
+    #print i, 'old neighbors:', graph[i]
+    for j in possibilities:#                            # in each potential subgroup of i's neighbors
+      for k in j:#                                      # for each member of that subgroup
+        graphprime.add_edge(i, k)#                      # connect it to (i)
+      if utility2(graphprime,i, delta, c) > utility2(winner, i, delta, c):
+        winner = copy.deepcopy(graphprime)
+        #print i, 'winner' , j
+      for k in j:
+        graphprime.remove_edge(i, k)#
+    graph.remove_edges_from(graph.edges())          
+    graph.add_edges_from(winner.edges())
+    #print i, 'new neighbors:', winner[i]               
+
+    
+def Find_Pairwise_Nash2(graph, delta, c, orderb = 3):
+  path = []
+  Done, Add, Cut = False, False, False
+  while Done == False:
+    if Add and Cut == True:
+      Done = True
+    while Add == False:
+      print "Adding:", time.clock()
+      edges = graph.edges()
+      One_Add2(graph, delta, c, orderb)
+      if graph.edges() == edges:
+        Add = True
+      else:
+        Cut = False ###Track outcomes, if improving cycle, break and retry with new order.
+        if nx.to_dict_of_lists(graph) in path:
+          Coin_Flip_Graph(graph, random.uniform(0,1))
+          print "Uh Oh, an improving cycle. Shuffling and trying again!"
+          Find_Pairwise_Nash2(graph, delta, c)
+          Add, Cut = True, True
+          break
+        else:
+          path.append(nx.to_dict_of_lists(graph))
+    while Cut == False:
+      print "Cutting:", time.clock()
+      Add = False
+      edges = graph.edges()
+      One_Cut2(graph, delta, c, orderb)
+      if graph.edges() == edges:
+        Cut = True
+      else:
+        Add = False
+        if nx.to_dict_of_lists(graph) in path:
+          Coin_Flip_Graph(graph, random.uniform(0,1))
+          print "Uh Oh, an improving cycle. Shuffling and trying again!"
+          Find_Pairwise_Nash2(graph, delta, c)
+          Add, Cut = True, True
+          break
+        else:
+          path.append(nx.to_dict_of_lists(graph))
